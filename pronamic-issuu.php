b316a8dad129b6e1142bd44053503fb7becac263
Remco Tolsma <info@remcotolsma.nl>
2012-10-04 11:01:18 +0200

    Added plugin file with plugin header.

---
+++ pronamic-issuu.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Pronamic Issuu
+Plugin URI: http://pronamic.eu/wp-plugins/issuu/
+Description: Easily integrate Issuu documents into WordPress.
+ 
+Version: 1.0
+Requires at least: 3.0
+
+Author: Pronamic
+Author URI: http://pronamic.eu/
+
+Text Domain: pronamic_issuu
+Domain Path: /languages/
+
+License: GPL
+
+GitHub URI: https://github.com/pronamic/wp-pronamic-issuu
+*/
+
